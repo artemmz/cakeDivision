0fb342f3d73b17d5b0a9efd6505139775f823b77
artemmz_ <artem825@mail.ru>
2015-03-30 01:19:20 +0300

    working version

--- src/birthday/CakeDivisor.java
+++ src/birthday/CakeDivisor.java
@@ -2,58 +2,148 @@ package birthday;
 
 import java.util.*;
 
+/**
+* CakeDivisor class helps to divide a cake (convex polygon)
+* into 4 equal parts with 2 perpendicular lines
+*/
 public class CakeDivisor{
 
-	static final double EPS = 1e-10; // not needed ????
+	static final double EPS = 1e-9; 
 	private static int vertexNumb; // total number of vertexes
 	private static List<Point> cake = new ArrayList<Point>();
+	private static Point center;
 	private static double cakeArea;
 	
 	public static void main(String[] args){
 		createCake();
 		cakeArea = area(cake);
-		System.out.println("area="+area(cake));
+		double minAngle = 0;
+		double maxAngle = Math.PI/2;
+		double curAngle = minAngle; 
+		double maxDiff = areaDiff(maxAngle);
+		double minDiff = areaDiff(minAngle);
+		boolean success = false;
+		if (Math.abs(minDiff) <= EPS*cakeArea){
+			success = true;
+		}
+		while (!success && Math.abs(maxDiff - minDiff) > EPS*cakeArea){
+			curAngle = (minAngle + maxAngle)/2;
+			double curDiff = areaDiff(curAngle);
+			if (curDiff == 0){
+				success = true;
+			}else if (	(minDiff > 0 && curDiff < 0) ||
+						(minDiff < 0 && curDiff > 0)	){
+				maxDiff = curDiff;
+				maxAngle = curAngle;
+			}else{
+				minDiff = curDiff;
+				minAngle = curAngle;
+			}
+		}
+		
+		System.out.printf("%f %f\n", center.x, center.y);
+		System.out.printf("%f\n",curAngle*180/Math.PI);
 	}
 	
 	private static void createCake(){
 		Scanner scan= new Scanner(System.in);
 		try{
 			vertexNumb = scan.nextInt();
+			if (vertexNumb < 3){
+				exitBirthday();
+			}
 			for (int i=0;i<vertexNumb;i++){
 				double x = scan.nextDouble();
 				double y = scan.nextDouble();
 				cake.add(new Point(x,y));
 			}
+			checkConvex(cake);
+			cake.add(cake.get(0));
 		}catch(InputMismatchException ex){
-			System.out.println("-1\nincorrect input"); 
+			exitBirthday();
 		}
+	}
+	
+	private static void checkConvex(List<Point> pol){
+		if (pol.size() == 3) return;
+		boolean sign = false;
+		int n = pol.size();
+		for (int i=0; i < n; i++){
+			double dx1 = pol.get((i+2)%n).x-pol.get((i+1)%n).x;
+		    double dy1 = pol.get((i+2)%n).y-pol.get((i+1)%n).y;
+		    double dx2 = pol.get(i).x-pol.get((i+1)%n).x;
+		    double dy2 = pol.get(i).y-pol.get((i+1)%n).y;
+		    double crossproduct = dx1*dy2 - dy1*dx2;
+		    if (i == 0){
+		    	sign = crossproduct > 0;
+		    }else{
+		    	if (sign != (crossproduct > 0)){
+		    		exitBirthday();
+		    	}
+		    }
+		}
+	}
+	
+	private static void exitBirthday(){
+		System.out.println("-1");
+		System.exit(0);
 	}
 	
 	private static double area(List<Point> pol){
 		double res = 0;
 		for (int i = 0; i < pol.size()-1; i++){
-			res +=	(pol.get(i + 1).x - pol.get(i).x) * 
-					(pol.get(i + 1).y + pol.get(i + 1).y);
+			res +=	(pol.get(i+1).x - pol.get(i).x) * 
+					(pol.get(i+1).y + pol.get(i).y);
 		}
 		return Math.abs(res)/2;
 	}
 	
+	private static double sqrDistance(Point p1,Point p2){
+		return Math.pow(p1.x - p2.x,2) + Math.pow(p1.y - p2.y,2);
+	}
+	
 	private static double areaDiff(double angle){
-		Point
+		Point[] firSect = bisectionPoints(angle);
+		Point[] secSect = bisectionPoints(angle+Math.PI/2);
+		center = linesInter(new Line(firSect[0],firSect[1]),
+							new Line(secSect[0],secSect[1]));
+		int stop = 0; //needed to find 1/4 
+		if (firSect[0].preVertex > secSect[0].preVertex){
+			stop = 1;
+		}else if(firSect[0].preVertex == secSect[0].preVertex){ 
+			// if perpendicular sections cross the same side
+			double firDist = sqrDistance(firSect[0],
+										 cake.get(firSect[0].preVertex));
+			double secDist = sqrDistance(secSect[0],
+										 cake.get(secSect[0].preVertex));
+			if (firDist > secDist){
+				stop = 1;
+			}
+		}
+		List<Point> quarterCake = new ArrayList<Point>();
+		quarterCake.add(center);
+		quarterCake.add(firSect[0]);
+		for (int i=firSect[0].preVertex + 1; i <= secSect[stop].preVertex; i++){
+			quarterCake.add(cake.get(i));
+		}
+		quarterCake.add(secSect[stop]);
+		quarterCake.add(center);  
+		return cakeArea/4 - area(quarterCake);
 	}
 	
 	private static Point[] bisectionPoints(double angle){
 		Point[] bPoints = new Point[2]; // always 2 intersection points
 		int lowNum = 0; //number of lowest vertex
-		double lowSect = getYCoord(cake,lowNum,angle); 
+		double lowSect = getYCoord(cake.get(lowNum),angle); 
 		double highSect = lowSect; // highest and lowest sections
 		double curSect;
 		for (int i=1; i<vertexNumb; i++){
-			curSect = getYCoord(cake,i,angle);
+			curSect = getYCoord(cake.get(i),angle);
 			if (curSect > highSect){
 				highSect = curSect;
 			}else if (curSect < lowSect){
 				lowSect = curSect;
+				lowNum = i;
 			}
 		}
 		
@@ -63,19 +153,19 @@ public class CakeDivisor{
 			curSect = (lowSect + highSect)/2;
 			double curY;
 			double nextY;
-			int interCount = 0; // number of intersections
-			for (int i=0; i < vertexNumb-1; i++){
-				curY = getYCoord(cake,i,angle) - curSect;
-				nextY = getYCoord(cake,i+1,angle) - curSect;
-				if (curY == 0){ // if section goes through vertex
-					Point intPoint = cake.get(i);
-					intPoint.preVertex = i;
-					bPoints[interCount] = intPoint;
-					interCount++;
-				}else if (curY*nextY < 0){ // if section crosses side
+			int interCount = 0;
+			for (int i=0; i < vertexNumb; i++){
+				curY = getYCoord(cake.get(i),angle) - curSect;
+				nextY = getYCoord(cake.get(i+1),angle) - curSect;
+				if (curY*nextY < 0){ // if section crosses side
 					Line side = new Line(cake.get(i),cake.get(i+1));
 					Line section = new Line(angle,curSect);
 					Point intPoint = linesInter(side,section);
+					intPoint.preVertex = i;
+					bPoints[interCount] = intPoint;
+					interCount++;
+				}else if (curY == 0){ // if section goes through vertex
+					Point intPoint = cake.get(i);
 					intPoint.preVertex = i;
 					bPoints[interCount] = intPoint;
 					interCount++;
@@ -89,9 +179,10 @@ public class CakeDivisor{
 				halfCake.add(cake.get(i));
 			}
 			halfCake.add(bPoints[1]);
+			halfCake.add(bPoints[0]); 
 			sMin = area(halfCake);
 			sMax = cakeArea - sMin;
-			if (lowNum > bPoints[0].preVertex && lowNum < bPoints[1].preVertex){
+			if (lowNum > bPoints[0].preVertex && lowNum <= bPoints[1].preVertex){
 				if (sMin > sMax){
 					highSect = curSect;
 				}else{
@@ -108,22 +199,21 @@ public class CakeDivisor{
 		return bPoints;
 	}
 	
-	private static Point linesInter(Line lOne,Line lTwo){
-		double denom = lOne.a*lTwo.b - lTwo.a*lOne.b;
+	private static Point linesInter(Line l1,Line l2){
+		double denom = l1.a*l2.b - l2.a*l1.b;
 		if (denom == 0){
-			System.out.println("-1\nyour cake is not convex!");
-			return null;
+			return null; //parallel
 		}else{
-			double x = (lOne.b*lTwo.c - lTwo.b*lOne.c)/denom;
-			double y = (lOne.c*lTwo.a - lTwo.c*lOne.a)/denom;
+			double x = (l1.b*l2.c - l2.b*l1.c)/denom;
+			double y = (l1.c*l2.a - l2.c*l1.a)/denom;
 			return new Point(x,y);
 		}
 	}
 	
-	private static double getYCoord(List<Point> pol,int vert,double angle){
+	private static double getYCoord(Point p,double angle){
 		double sinA = Math.sin(angle);
 		double cosA = Math.cos(angle);
-		return sinA*pol.get(vert).x + cosA*pol.get(vert).y; 
+		return sinA*p.x - cosA*p.y; 
 	}
 	
 	private static class Point{
@@ -143,10 +233,10 @@ public class CakeDivisor{
 		public double c; // ax+by+c=0
 		
 //		(y1-y2)x+(x2-x1)y+(x1y2-x2y1) = 0 
-		public Line(Point pOne,Point pTwo){
-			this.a = pOne.y - pTwo.y;
-			this.b = pTwo.x - pOne.x;
-			this.c = pOne.x*pTwo.y - pTwo.x*pOne.y;
+		public Line(Point p1,Point p2){
+			this.a = p1.y - p2.y;
+			this.b = p2.x - p1.x;
+			this.c = p1.x*p2.y - p2.x*p1.y;
 		}
 		
 //		y=kx+b --> -kx+y-b=0 --> ax+by+c=0
@@ -158,7 +248,7 @@ public class CakeDivisor{
 			}else{
 				this.a = -Math.tan(angle);
 				this.b = 1;
-				this.c = -perpend/Math.cos(angle);
+				this.c = perpend/Math.cos(angle);
 			}
 		}
 	}
